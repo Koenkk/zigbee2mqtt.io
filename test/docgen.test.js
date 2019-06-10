17c3430b631bf81b3b255eedc81e80bd1a70454b
Koen Kanters <koenkanters94@gmail.com>
2019-06-10 17:53:15 +0200

    Fix tests

--- test/docgen.test.js
+++ test/docgen.test.js
@@ -5,22 +5,12 @@ const path = require('path');
 const base = path.join(__dirname, '..');
 
 const supportDevices = require('../docgen/supported-devices');
-const integratingWithHomeassistant = require('../docgen/integrating-with-homeassistant');
 
 describe('Docgen', () => {
     it('supported-devices.md should be up-to-date.', () => {
         const actual = fs.readFileSync(path.join(base, 'information', 'supported_devices.md')).toString();
         chai.assert.strictEqual(
             supportDevices,
-            actual,
-            'supported-devices.md is not up-to-date, forgot to run npm run docgen?'
-        );
-    });
-
-    it('integrating-with-homeassistant.md should be up-to-date.', () => {
-        const actual = fs.readFileSync(path.join(base, 'integration', 'home_assistant.md')).toString();
-        chai.assert.strictEqual(
-            integratingWithHomeassistant,
             actual,
             'supported-devices.md is not up-to-date, forgot to run npm run docgen?'
         );
