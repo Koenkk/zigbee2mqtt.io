b16c838bb90168f9f155a68d093f66691aac32a0
Koen Kanters <koenkanters94@gmail.com>
2020-05-21 11:51:56 +0200

    ZNCZ04LM power_outage_memory docs. https://github.com/Koenkk/zigbee-herdsman-converters/pull/1250

--- docgen/device_page_notes.js
+++ docgen/device_page_notes.js
@@ -1247,7 +1247,7 @@ See [link](https://github.com/Koenkk/zigbee2mqtt/issues/2077#issuecomment-538691
 `,
     },
     {
-        model: ['ZNCZ02LM'],
+        model: ['ZNCZ02LM', 'ZNCZ04LM'],
         note: `
 ### Power outage memory
 This option allows the device to restore the last on/off state when it's reconnected to power.
