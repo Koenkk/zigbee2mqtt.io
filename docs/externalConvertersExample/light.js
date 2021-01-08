098fa42fb55fbe891e8f873a531be06481d15e62
Koen Kanters <koenkanters94@gmail.com>
2021-01-08 20:35:29 +0100

    Update external converters example. https://github.com/Koenkk/zigbee2mqtt/issues/5586

---
+++ docs/externalConvertersExample/light.js
@@ -0,0 +1,16 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const e = exposes.presets;
+
+const device = {
+    zigbeeModel: ['myZigbeeModel'],
+    model: 'myModel',
+    vendor: 'myFactory',
+    description: 'My super lamp!',
+    fromZigbee: [fz.on_off, fz.brightness, fz.ignore_basic_report, fz.color_colortemp],
+    toZigbee: [tz.light_onoff_brightness, tz.ignore_transition, tz.ignore_rate, tz.effect, tz.light_brightness_move, tz.light_brightness_step, tz.light_colortemp, tz.light_colortemp_move, tz.light_colortemp_step, tz.light_hue_saturation_move, tz.light_hue_saturation_step, tz.light_color],
+    exposes: [e.light_brightness_colortemp_colorxy()],
+};
+
+module.exports = device;
