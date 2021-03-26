0c7e258796246f679b722969ac8be8497248aca1
Koen Kanters <koenkanters94@gmail.com>
2021-03-26 18:33:28 +0100

    Add tz.read to light external converter example

--- docs/externalConvertersExample/light.js
+++ docs/externalConvertersExample/light.js
@@ -10,7 +10,7 @@ const device = {
     vendor: 'myFactory',
     description: 'My super lamp!',
     fromZigbee: [fz.on_off, fz.brightness, fz.ignore_basic_report, fz.color_colortemp],
-    toZigbee: [tz.light_onoff_brightness, tz.ignore_transition, tz.ignore_rate, tz.effect, tz.light_brightness_move, tz.light_brightness_step, tz.light_colortemp, tz.light_colortemp_move, tz.light_colortemp_step, tz.light_hue_saturation_move, tz.light_hue_saturation_step, tz.light_color],
+    toZigbee: [tz.light_onoff_brightness, tz.ignore_transition, tz.ignore_rate, tz.effect, tz.light_brightness_move, tz.light_brightness_step, tz.light_colortemp, tz.light_colortemp_move, tz.light_colortemp_step, tz.light_hue_saturation_move, tz.light_hue_saturation_step, tz.light_color, tz.read],
     exposes: [e.light_brightness_colortemp_colorxy()],
 };
 
