9efba9ce07aaf9dbd44283f242056cf3bfa01810
Koen Kanters <koenkanters94@gmail.com>
2021-04-11 13:14:43 +0200

    const ea = exposes.access;

--- docs/externalConvertersExample/light.js
+++ docs/externalConvertersExample/light.js
@@ -3,6 +3,7 @@ const tz = require('zigbee-herdsman-converters/converters/toZigbee');
 const exposes = require('zigbee-herdsman-converters/lib/exposes');
 const reporting = require('zigbee-herdsman-converters/lib/reporting');
 const e = exposes.presets;
+const ea = exposes.access;
 
 const device = {
     zigbeeModel: ['myZigbeeModel'],
