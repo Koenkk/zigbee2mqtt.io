ce0f892526377d243277456cee17af02e4207471
Ainur Timerbaev <ainur.timerbaev@superawesome.com>
2024-01-30 12:00:56 +0000

    Allow 512x512 images

--- docgen/tests/check-device-image-size.ts
+++ docgen/tests/check-device-image-size.ts
@@ -6,19 +6,19 @@ import * as path from "path";
 
 const sizeOfP = promisify(sizeOf);
 const ignore = ['.DS_Store'];
-
+const allowedDimensions = ['150x150', '512x512'];
 export async function checkDeviceImageSize() {
-  console.log('* Test if device images has 150x150px dimensions...');
+  console.log(`* Test if device images has ${allowedDimensions.join(' or ')}  dimensions...`);
   const invalid = [];
   const images = (await fsp.readdir(imageBaseDir)).filter((f) => !ignore.includes(f));
   await Promise.all(images.map(async img => {
     const dimensions = await sizeOfP(path.resolve(imageBaseDir, img));
-    if (dimensions.width != 150 || dimensions.height != 150) {
+    if (!allowedDimensions.includes(`${dimensions.width}x${dimensions.height}`)) {
       invalid.push(img);
     }
   }));
 
   if(invalid.length) {
-    throw invalid.reduce((res, d) => res += `Image ${ d } does not have 150x150px\n`, "");
+    throw invalid.reduce((res, d) => res += `Image ${ d } does not have ${allowedDimensions.join(' or ')}\n`, "");
   }
 }
