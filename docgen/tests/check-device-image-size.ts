80f80f9bbe541baacda5043877accfe70aeffcc6
John Doe <nurikk@users.noreply.github.com>
2024-01-30 12:38:35 +0000

    More verbose output

--- docgen/tests/check-device-image-size.ts
+++ docgen/tests/check-device-image-size.ts
@@ -9,16 +9,17 @@ const ignore = ['.DS_Store'];
 const allowedDimensions = ['150x150', '512x512'];
 export async function checkDeviceImageSize() {
   console.log(`* Test if device images has ${allowedDimensions.join(' or ')}  dimensions...`);
-  const invalid = [];
+  const invalid = {};
   const images = (await fsp.readdir(imageBaseDir)).filter((f) => !ignore.includes(f));
   await Promise.all(images.map(async img => {
     const dimensions = await sizeOfP(path.resolve(imageBaseDir, img));
-    if (!allowedDimensions.includes(`${dimensions.width}x${dimensions.height}`)) {
-      invalid.push(img);
+    const size = `${dimensions.width}x${dimensions.height}`;
+    if (!allowedDimensions.includes(size)) {
+      invalid[img] = size;
     }
   }));
 
-  if(invalid.length) {
-    throw invalid.reduce((res, d) => res += `Image ${ d } does not have ${allowedDimensions.join(' or ')}\n`, "");
+  if(Object.keys(invalid).length) {
+    throw Object.entries(invalid).reduce((res, current_image_size_pair) => res += `Image ${ current_image_size_pair[0] } does not have ${allowedDimensions.join(' or ')} (size: ${current_image_size_pair[1]})\n`, "");
   }
 }
