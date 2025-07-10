38091100815471a727872d931bf2ff6a8d38ad26
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-07-10 17:16:11 +0200

    add header, fix display issues, add example code blocks

--- docgen/generate_settings.ts
+++ docgen/generate_settings.ts
@@ -1,8 +1,30 @@
 import type {JSONSchema7} from 'json-schema';
 import {generatePage} from './utils';
 
-const displayProperty = (key: string, property: JSONSchema7, depth: number, required: boolean): string => {
-    let str = `${'#'.repeat(depth + 2)} ${key} ${required ? '<span style="color: red">*</span>' : ''}`;
+const getDisplayValue = (
+    property: JSONSchema7,
+    fallback: string | number | boolean | object,
+    defaultOverride: JSONSchema7['default'] = undefined,
+): string => {
+    // in order: default override, default, first example, first enum, min, max
+    const pDefault = defaultOverride ?? property.default;
+
+    if (pDefault != null) {
+        return JSON.stringify(pDefault);
+    }
+
+    return `${Array.isArray(property.examples) && property.examples.length > 0 ? JSON.stringify(property.examples[0]) : property.enum && property.enum.length > 0 ? JSON.stringify(property.enum[0]) : (property.minimum ?? property.maximum ?? JSON.stringify(fallback))}`;
+};
+
+const displayProperty = (
+    key: string,
+    property: JSONSchema7,
+    parents: string[],
+    required: boolean,
+    defaultOverride: JSONSchema7['default'] = undefined,
+): string => {
+    const pDefault = defaultOverride ?? property.default;
+    let str = `${'#'.repeat(parents.length + 2)} ${key} ${required ? '<span style="color: red">*</span>' : ''}`;
 
     if (!property.description && property.title) {
         str += `\r\n\r\n${property.title}`;
@@ -12,25 +34,77 @@ const displayProperty = (key: string, property: JSONSchema7, depth: number, requ
         str += `\r\n\r\n${property.description}`;
     }
 
-    str += `\r\n- Type: \`${property.type}\` ${property.type === 'array' && typeof property.items === 'object' && 'type' in property.items ? `of \`${property.items.type}\`` : ''}`;
+    str += `\r\n\r\n\`\`\`yaml`;
+    let i = 0;
 
-    if (property.default) {
-        str += `\r\n- Default: \`${property.default}\``;
+    for (const parent of parents) {
+        str += `\r\n${' '.repeat(i * 4)}${parent}:`;
+        i++;
     }
 
-    if (property.enum) {
+    str += `\r\n${' '.repeat(i * 4)}${key}:`;
+
+    let pType = property.type;
+
+    if (Array.isArray(property.type)) {
+        const nonNull = property.type.filter((t) => t !== 'null');
+
+        if (nonNull.length === 1) {
+            pType = pDefault == null ? 'null' : nonNull[0];
+        }
+    }
+
+    switch (pType) {
+        case 'boolean': {
+            str += ` ${getDisplayValue(property, true)}`;
+            break;
+        }
+        case 'number':
+        case 'integer': {
+            str += ` ${getDisplayValue(property, 0)}`;
+            break;
+        }
+        case 'string': {
+            str += ` ${getDisplayValue(property, 'abc')}`;
+            break;
+        }
+        case 'array': {
+            str += ` ${getDisplayValue(property, [])}`;
+            break;
+        }
+        case 'object': {
+            str += ` ${getDisplayValue(property, {})}`;
+            break;
+        }
+        case 'null': {
+            str += ' null';
+            break;
+        }
+    }
+
+    str += `\r\n\`\`\`\r\n`;
+
+    str += `\r\n- Type: \`${property.type}\` ${property.type === 'array' && typeof property.items === 'object' && !Array.isArray(property.items) ? `of \`${property.items.type}\`` : ''}`;
+
+    if (pDefault != null) {
+        str += `\r\n- Default: \`${JSON.stringify(pDefault)}\``;
+    }
+
+    if (property.enum != null) {
         str += `\r\n- Possible values: ${property.enum.map((v) => `\`${v}\``).join(', ')}`;
+    } else if (property.type === 'array' && typeof property.items === 'object' && !Array.isArray(property.items) && property.items.enum) {
+        str += `\r\n- Possible array values: ${property.items.enum.map((v) => `\`${v}\``).join(', ')}`;
     }
 
-    if (property.minimum) {
+    if (property.minimum != null) {
         str += `\r\n- Minimum: \`${property.minimum}\``;
     }
 
-    if (property.maximum) {
+    if (property.maximum != null) {
         str += `\r\n- Maximum: \`${property.maximum}\``;
     }
 
-    if (property.examples) {
+    if (property.examples != null) {
         if (Array.isArray(property.examples)) {
             str += `\r\n- Examples:`;
 
@@ -47,7 +121,7 @@ const displayProperty = (key: string, property: JSONSchema7, depth: number, requ
 
 const groupProperties = (
     properties: JSONSchema7['properties'],
-    depth: number,
+    parents: string[] = [],
     required: string[] = [],
     definitions: Record<string, JSONSchema7> = {},
 ): string[] => {
@@ -61,38 +135,59 @@ const groupProperties = (
             continue;
         }
 
+        const newParents = parents.slice();
+
+        newParents.push(key);
+
         if (property.properties) {
             nestedElements.push(`
-${'#'.repeat(depth + 2)} ${key}
+${'#'.repeat(parents.length + 2)} ${key}
+
 ${property.title || ''}
 
-${groupProperties(property.properties, depth + 1, property.required).join('\r\n')}`);
+${groupProperties(property.properties, newParents, property.required, definitions).join('\r\n')}`);
         } else {
-            const oneOf = property.oneOf?.find((one) => typeof one === 'object' && one.properties) as JSONSchema7 | undefined;
+            if (property.oneOf) {
+                const oneOf = property.oneOf?.find((one) => typeof one === 'object' && one.properties) as JSONSchema7 | undefined;
 
-            if (oneOf) {
-                const others = property.oneOf?.filter((one) => typeof one === 'object' && !one.properties) || [];
-
-                nestedElements.push(`
-${'#'.repeat(depth + 2)} ${key}
-${oneOf.title || property.title || ''}
-
-${groupProperties(oneOf.properties, depth + 1, oneOf.required).join('\r\n')}`);
-
-                for (const other of others) {
-                    nestedElements.push(displayProperty(key, other as JSONSchema7, depth, required.includes(key)));
-                }
-            } else {
-                if (depth === 0 && (key === 'devices' || key === 'groups')) {
-                    const definition = definitions[key.slice(0, -1)];
+                if (oneOf) {
+                    const others = property.oneOf?.filter((one) => typeof one === 'object' && !one.properties) || [];
 
                     nestedElements.push(`
-${'#'.repeat(depth + 2)} ${key}
+    ${'#'.repeat(parents.length + 2)} ${key}
+
+    ${oneOf.title || property.title || ''}
+
+    ${groupProperties(oneOf.properties, newParents, oneOf.required, definitions).join('\r\n')}`);
+
+                    for (const other of others) {
+                        // override default (in parent, not in oneOf)
+                        nestedElements.push(displayProperty(key, other as JSONSchema7, parents, required.includes(key), property.default));
+                    }
+                } else {
+                    for (const oneOfI of property.oneOf) {
+                        // override default (in parent, not in oneOf)
+                        nestedElements.push(displayProperty(key, oneOfI as JSONSchema7, parents, required.includes(key), property.default));
+                    }
+                }
+            } else {
+                if (parents.length === 0 && (key === 'devices' || key === 'groups')) {
+                    const definition = definitions[key.slice(0, -1)];
+
+                    if (key === 'devices') {
+                        newParents.push('"0x1234567812345678"');
+                    } else {
+                        newParents.push('1');
+                    }
+
+                    nestedElements.push(`
+${'#'.repeat(parents.length + 2)} ${key}
+
 ${property.title || ''}
 
-${groupProperties(definition.properties, depth + 1, definition.required).join('\r\n')}`);
+${groupProperties(definition.properties, newParents, definition.required, definitions).join('\r\n')}`);
                 } else {
-                    elements.push(displayProperty(key, property, depth, required.includes(key)));
+                    elements.push(displayProperty(key, property, parents, required.includes(key)));
                 }
             }
         }
@@ -117,7 +212,15 @@ pageClass: settings-page
 <!-- ATTENTION: This file is auto-generated through docgen, do not edit! -->
 <!-- !!!! -->
 
-${groupProperties(schemaJson.properties, 0, schemaJson.required, schemaJson.definitions).sort().join('\r\n')}
+# Settings
+
+This page contains all currently supported settings in \`configuration.yaml\`.
+
+::: tip NOTE
+The code blocks show an **example value** for each setting (may be the default or any value derived from the possibilities/examples/boundaries).
+:::
+
+${groupProperties(schemaJson.properties, [], schemaJson.required, schemaJson.definitions).sort().join('\r\n')}
 `;
 
     return generatePage(result, 'docs/guide/settings.md');
