15787b0061ae61ee60218eb7bcce18573dd44cde
Andy <andrei_lazarov@yahoo.com>
2026-07-14 20:01:21 +0300

    Recommend Shelly firmware updates (#5323)

---
+++ docgen/device_page_warnings/shelly_firmware.ts
@@ -0,0 +1,14 @@
+import {Definition} from 'zigbee-herdsman-converters';
+
+export function shellyFirmware(definition: Definition) {
+    if (definition.vendor === 'Shelly') {
+        return `
+## Firmware
+
+It is recommended to connect Shelly devices by WiFi / Bluetooth, and update their firmware, until they gain support for OTA updates over Zigbee.
+
+The latest firmware fixes known issues like negative power readings on some models.  
+*Note they roll-out updates in phases. Check "beta" channels if you are specifically looking for a fix.*
+`;
+    }
+}
